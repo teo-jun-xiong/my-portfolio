7bfc4177c4b573cb07f52fccdd1d046130629fba
teo-jun-xiong <teojunxiong97@gmail.com>
2020-07-03 12:23:02 +0800

    Updates

---
+++ precache-manifest.c6bc9723111a50035ab3f934a5e6c5da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd75a408b03578ee8ff0308891169ccd",
+    "url": "/portfolio-v2/index.html"
+  },
+  {
+    "revision": "ad5ca2d12ba11ea23af0",
+    "url": "/portfolio-v2/static/js/2.75fba282.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-v2/static/js/2.75fba282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "831a066ce726b7af68eb",
+    "url": "/portfolio-v2/static/js/main.4c715960.chunk.js"
+  },
+  {
+    "revision": "58daaf2044f28a9eccfa",
+    "url": "/portfolio-v2/static/js/runtime-main.a8d6ddc0.js"
+  }
+]);
