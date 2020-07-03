fbfaf83727b332a3336dcacd7f1e69f99b0fb808
teo-jun-xiong <teojunxiong97@gmail.com>
2020-07-03 12:48:26 +0800

    Updates

--- precache-manifest.d931816d852906123cbb579cc760e1a0.js
+++ precache-manifest.d931816d852906123cbb579cc760e1a0.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cd75a408b03578ee8ff0308891169ccd",
+    "revision": "a9ad843aac6f9d8930b68b6f2ea609c8",
     "url": "/portfolio-v2/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio-v2/static/js/2.75fba282.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "831a066ce726b7af68eb",
-    "url": "/portfolio-v2/static/js/main.4c715960.chunk.js"
+    "revision": "acd042288751e7f9ce34",
+    "url": "/portfolio-v2/static/js/main.3840497f.chunk.js"
   },
   {
     "revision": "58daaf2044f28a9eccfa",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-v2/precache-manifest.c6bc9723111a50035ab3f934a5e6c5da.js"
+  "/portfolio-v2/precache-manifest.d931816d852906123cbb579cc760e1a0.js"
 );
 
 self.addEventListener('message', (event) => {
