ec546a5d9dfab8e056b84d270a5953671e464b37
teo-jun-xiong <teojunxiong97@gmail.com>
2020-07-03 18:28:14 +0800

    Shift NavigationBar into Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
-import NavigationBar from './NavigationBar';
 import programmer from "../images/programmer.png";
 import cloud from "../images/cloud.png";
 import mountain from "../images/mountain.png";
 
 function Header() {
     return <div className="div-header">
-        <NavigationBar />
+    <nav className="navbar navbar-expand-lg my-navbar navbar-dark">
+            <a className="navbar-brand" href="#div-header">
+                <img src={programmer} width="50px"/>
+            </a>
+
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+                    <li className=" nav-item active">
+                        <a className="nav-link" href="#about-me">About Me<span className="sr-only"></span></a>
+                    </li>
+                    <li className=" nav-item active">
+                        <a className="nav-link" href="#education">Education<span className="sr-only"></span></a>
+                    </li>
+                    <li className=" nav-item active">
+                        <a className="nav-link" href="#projects">Projects<span className="sr-onl "></span></a>
+                    </li>
+                    <li className=" nav-item active">
+                        <a className="nav-link" href="#jobs">Job Experience<span className="sr-only"></span></a>
+                    </li>
+                    <li className=" nav-item active">
+                        <a className="nav-link" href="#skills">Skills<span className="sr-only"></span></a>
+                    </li>
+                    <li className=" nav-item active">
+                        <a className="nav-link" href="#links">Links<span className="sr-only"></span></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
         <br/><br/>
         <nav className="navbar navbar-expand-lg my-navbar navbar-dark">
