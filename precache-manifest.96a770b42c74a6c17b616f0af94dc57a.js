dec1dc9a8dbdd6bcc3765a064ce4103300ef607b
teo-jun-xiong <teojunxiong97@gmail.com>
2020-09-29 15:27:02 +0800

    Updates

---
+++ precache-manifest.96a770b42c74a6c17b616f0af94dc57a.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944d355e95f5241e472e46dfb1e66f88",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "6840ccc617f93b43a73f",
+    "url": "/my-portfolio/static/css/main.0841a9ad.chunk.css"
+  },
+  {
+    "revision": "365c7ea0fd38bceb4760",
+    "url": "/my-portfolio/static/js/2.c2ef77b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.c2ef77b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6840ccc617f93b43a73f",
+    "url": "/my-portfolio/static/js/main.bedc35d1.chunk.js"
+  },
+  {
+    "revision": "e3b8734abc7478bd1ce9",
+    "url": "/my-portfolio/static/js/runtime-main.53f511a6.js"
+  },
+  {
+    "revision": "86ed6943a51308aab787481ebb1f48f0",
+    "url": "/my-portfolio/static/media/cooking-papa.86ed6943.png"
+  },
+  {
+    "revision": "75e4d6d5a0f6b8f4e4030f9b00511ca7",
+    "url": "/my-portfolio/static/media/css.75e4d6d5.png"
+  },
+  {
+    "revision": "09fa537f9f9a2b0c0397e1d4b09455fa",
+    "url": "/my-portfolio/static/media/education.09fa537f.png"
+  },
+  {
+    "revision": "1c6226cee344eec148a0fbddf9eb4dc5",
+    "url": "/my-portfolio/static/media/eldereach.1c6226ce.png"
+  },
+  {
+    "revision": "b9ddd9855c0240558874d356aa377ec4",
+    "url": "/my-portfolio/static/media/email.b9ddd985.png"
+  },
+  {
+    "revision": "446f74f78e8783995aea058db0870aa5",
+    "url": "/my-portfolio/static/media/git.446f74f7.png"
+  },
+  {
+    "revision": "0918d78648457def080137b179fc5608",
+    "url": "/my-portfolio/static/media/github.0918d786.png"
+  },
+  {
+    "revision": "5616336638bf4d4284ffa0ad8fd1d219",
+    "url": "/my-portfolio/static/media/help.56163366.png"
+  },
+  {
+    "revision": "31039a53ab57f00b4eff33fb7bcfa2e8",
+    "url": "/my-portfolio/static/media/html.31039a53.png"
+  },
+  {
+    "revision": "74e943d82452f81f6a0bb0b51ac6785d",
+    "url": "/my-portfolio/static/media/java.74e943d8.png"
+  },
+  {
+    "revision": "6a4c201cef84c174d0d93923f26b168c",
+    "url": "/my-portfolio/static/media/javascript.6a4c201c.png"
+  },
+  {
+    "revision": "e0695d69c143271f655355c456187f55",
+    "url": "/my-portfolio/static/media/linkedin.e0695d69.png"
+  },
+  {
+    "revision": "5873391ddff6110f2f6429fa23ae5994",
+    "url": "/my-portfolio/static/media/mountain-bg.5873391d.png"
+  },
+  {
+    "revision": "e8ce8699ffd31964e29bba44e3ca5b7e",
+    "url": "/my-portfolio/static/media/ping-pong.e8ce8699.png"
+  },
+  {
+    "revision": "ac18bd356a735086b95e7e8a017e1383",
+    "url": "/my-portfolio/static/media/programmer.ac18bd35.png"
+  },
+  {
+    "revision": "475ff612212f7276d5f36606e41471e3",
+    "url": "/my-portfolio/static/media/project-carousel-cooking-papa-1.475ff612.png"
+  },
+  {
+    "revision": "778f04d5c8fc4fde1349ed67cf1a8e2f",
+    "url": "/my-portfolio/static/media/project-carousel-eldereach-1.778f04d5.png"
+  },
+  {
+    "revision": "560cc40ac49e7bab0e2c8b41a2c1f160",
+    "url": "/my-portfolio/static/media/project-carousel-eldereach-2.560cc40a.png"
+  },
+  {
+    "revision": "443fd464a35baf7a46c317f44fc81906",
+    "url": "/my-portfolio/static/media/project-carousel-eldereach-3.443fd464.png"
+  },
+  {
+    "revision": "e5d0250175f3f265a9ca2c15d96802a1",
+    "url": "/my-portfolio/static/media/project-carousel-spreent-1.e5d02501.jpg"
+  },
+  {
+    "revision": "4c57ca156109c24347718e34431c0b4f",
+    "url": "/my-portfolio/static/media/project-carousel-spreent-2.4c57ca15.jpg"
+  },
+  {
+    "revision": "3ee1b9dac5d91f92315f9ce0b56fedf1",
+    "url": "/my-portfolio/static/media/project-carousel-wing-it-1.3ee1b9da.png"
+  },
+  {
+    "revision": "7e78dd538f8cba5d9aa405ac735cbda3",
+    "url": "/my-portfolio/static/media/project-carousel-wing-it-2.7e78dd53.png"
+  },
+  {
+    "revision": "66d7fb3367cab4e9d607048c5582df52",
+    "url": "/my-portfolio/static/media/project.66d7fb33.png"
+  },
+  {
+    "revision": "182e98238de8c18dd7974a6cb32a13c2",
+    "url": "/my-portfolio/static/media/python.182e9823.png"
+  },
+  {
+    "revision": "f297dfcd8b3e9215ee862f232d5c9d13",
+    "url": "/my-portfolio/static/media/react.f297dfcd.png"
+  },
+  {
+    "revision": "c9dadcfa1e17de82b1fd490e1be8f9d0",
+    "url": "/my-portfolio/static/media/skills.c9dadcfa.png"
+  },
+  {
+    "revision": "844c32c734531db59c71bf6a52d9421d",
+    "url": "/my-portfolio/static/media/sound.844c32c7.png"
+  },
+  {
+    "revision": "211026a3c59eddf63b9e3212cc6d54a7",
+    "url": "/my-portfolio/static/media/spreent.211026a3.png"
+  },
+  {
+    "revision": "7eafec610d9e04060c6ee973ade8ff3c",
+    "url": "/my-portfolio/static/media/web-page.7eafec61.png"
+  },
+  {
+    "revision": "2a8d51ddc8914eb5828f4d19c10bff26",
+    "url": "/my-portfolio/static/media/wing-it.2a8d51dd.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-v2/precache-manifest.143792b5d02f7d2c577431a38031441f.js"
+  "/my-portfolio/precache-manifest.96a770b42c74a6c17b616f0af94dc57a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-v2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
